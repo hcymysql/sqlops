9b38f3a75ff93a64208a1d99ebdf23c020f3bb38
hcymysql <chunyang_he@139.com>
2020-03-31 14:11:32 +0800

    Update send_reset_pass_mail.php

--- send_reset_pass_mail.php
+++ send_reset_pass_mail.php
@@ -23,7 +23,7 @@ if (mysqli_num_rows($result) > 0){
 
 $String = base64_encode($username.",".$useremail.",".$find_pwd_time);
 
-system("./mail/sendEmail -f sqlops@126.com -t {$useremail} -s smtp.126.com -u '你的密码找回信' -o message-charset=utf8 -o message-content-type=html -m '请在10分钟内重置密码，过期链接失效。请不要把这个链接告诉别人，你懂的！</br></br><a href='http://sqlops.xxx.com/sqlops_approve/resetUserPass.php?p={$String}'>确认密码找回</a>' -xu sqlops@126.com  -xp 'uGSacaObzf14o957'  ");
+system("./mail/sendEmail -f sqlops@126.com -t {$useremail} -s smtp.126.com -u '你的密码找回信' -o message-charset=utf8 -o message-content-type=html -m '请在10分钟内重置密码，过期链接失效。请不要把这个链接告诉别人，你懂的！</br></br><a href='http://sqlops.xxx.com/sqlops_approve/resetUserPass.php?p={$String}'>确认密码找回</a>' -xu sqlops@126.com  -xp '123456'  ");
 
 echo "</br></br>";
 echo "请登录你的邮箱：".$useremail."查看修改密码步骤。</br></br>";
