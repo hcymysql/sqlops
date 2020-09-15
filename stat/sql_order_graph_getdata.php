998bc0608910b08aac088af90b52eb0bf44ce81e
hcymysql <chunyang_he@139.com>
2020-09-15 17:12:22 +0800

    Add files via upload

--- stat/sql_order_graph_getdata.php
+++ stat/sql_order_graph_getdata.php
@@ -3,37 +3,18 @@
 //function index($arr1){
 function index(){
     ini_set('date.timezone','Asia/Shanghai');
-    /*
-    $ip = $_GET['ip'];
-    $dbname = $_GET['dbname'];
-    $port = $_GET['port'];
-    */
-   
-    /*
-    $ip = '192.168.155.30';
-    $dbname = 'nirvana';
-    $port = '3307';
-    */
-/*
-    $ip = $arr1;
-    $dbname = $arr2;
-    $port = $arr3;
-*/
 
-    $con = mysqli_connect("192.168.198.242","admin","wdhcy159753","sql_db") or die("数据库链接错误".mysql_error());
-    mysqli_query($con,"set names utf8");
-    //$get_info="select DATE_FORMAT(ops_time,'%Y-%m') AS stat_month,count(*) as counts from sql_order_wait where ops_time >=concat(DATE_FORMAT(DATE_SUB(now(),interval 1 month),'%Y-%m'),'-01') group by DATE_FORMAT(ops_time,'%Y-%m')";
+    require '../conn.php';
     $get_info="select DATE_FORMAT(ops_time,'%Y-%m') AS stat_month,count(*) as counts from sql_order_wait where ops_time >=concat(DATE_FORMAT(now(),'%Y'),'-01-01') group by DATE_FORMAT(ops_time,'%Y-%m')";
-
-    $result1 = mysqli_query($con,$get_info);
-	//echo $get_info;
+	
+    $result1 = mysqli_query($conn,$get_info);
 
   $array= array();
   class Connections{
     public $stat_month;
     public $counts;
   }
-  while($row = mysqli_fetch_array($result1,MYSQL_ASSOC)){
+  while($row = mysqli_fetch_array($result1,MYSQLI_ASSOC)){
     $cons=new Connections();
     $cons->stat_month = $row['stat_month'];
     //$user->user_max = $row['user_max'];
