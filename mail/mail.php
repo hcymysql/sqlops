30d22ba84740b36c40cd91ab7f3e83df3b178f89
hcymysql <chunyang_he@139.com>
2020-09-16 14:56:17 +0800

    Update mail.php

--- mail/mail.php
+++ mail/mail.php
@@ -18,6 +18,8 @@ class mail{
 	}
 	
 	function execCommand(){
+		//$SUBJECT_NAME=`echo -n 数据运维工单处理提醒 | base64`;
+                //$SUBJECT="=?utf-8?b?{$SUBJECT_NAME}?=";
 		$SUBJECT='数据运维工单处理提醒';
 		system("./mail/sendEmail -f chunyang_he@126.com -t {$this->approver} -cc {$this->dev_user_mail} -s smtp.126.com:25 -u '$SUBJECT' -o message-charset=utf8 -o message-content-type=html -m '工单号：{$this->order_id} <br>工单名称：{$this->sql_order} <br>工单提交人：{$this->dev_user} <br>数据库名：{$this->dbname} <br>需要您进行审核及实施，请登录<a href='http://sqlops.xxx.com/sqlops_approve/'>SQL自动审核平台</a>查看并处理！' -xu chunyang_he@126.com  -xp '123456' -o tls=no");
 	}
